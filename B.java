3bf446cacbeab4ac062a64906bca24d06cbbb39f
Ching Han Huang <tibette0817>
2020-02-11 17:10:15 -0500

    Extend B and modify Driver to show it

---
+++ B.java
@@ -0,0 +1,17 @@
+public class B{
+    protected int data;
+
+    public B(){
+        this(0);
+    }
+
+    public B(int data){
+        this.data = data;
+    }
+
+    @Override
+    public String toString(){
+        return new String("Value of data in B is: "
+                                    + Integer.toString(data));
+    }
+}
